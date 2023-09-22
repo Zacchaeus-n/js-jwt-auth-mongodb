b69d5df2b927ab4dd2e8d48209fdc0f519708858
Zacchaeus Napuo <82449341+Zacchaeus-n@users.noreply.github.com>
2023-09-22 20:06:38 +0000

    mongo commands

--- server.js
+++ server.js
@@ -83,3 +83,10 @@ function initial() {
       }
     });
   }
+
+
+  /**
+   * brew services start mongodb-community@7.0
+   * brew services stop mongodb-community@7.0
+   * mongod --config /usr/local/etc/mongod.conf --fork
+   */
